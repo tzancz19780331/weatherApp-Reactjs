a26c5e3bd329a7dd99c93cc916f05173c8dba44a
tzancz19780331 <tzancz197831@gmail.com>
2022-06-25 00:20:02 +0200

    z

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "{753a9cd860d1ebf5af315d064df621f5}",
   base: "https://api.openweathermap.org/data/2.5/",
 };
